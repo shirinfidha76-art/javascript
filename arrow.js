766d26914f8324a486a14cdcb09bf1d109f8e7d3
Fidha Shirin <shirinfidha76@gmail.com>
2026-07-07 11:25:47 +0530

    arrow added

---
+++ arrow.js
@@ -0,0 +1,11 @@
+//arrow
+let number1=20;
+let number2=20;
+const addTwoNumbers=(n1,n2)=>{
+    let sum=n1+n2;
+    return sum
+}
+let result=addTwoNumbers(number1,number2);
+ console.log("first number:",number1)
+    console.log("second number:",number2)
+    console.log("sum:",result);
