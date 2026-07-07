1135862d784d30559eed393a3810a0458b684c89
Fidha Shirin <shirinfidha76@gmail.com>
2026-07-07 11:21:28 +0530

    expression added

---
+++ expression.js
@@ -0,0 +1,11 @@
+//expression
+let number1=10;
+let number2=20;
+const addTwoNumbers=function(n1,n2){
+    let sum=n1+n2;
+    return sum
+}
+let result=addTwoNumbers(number1,number2);
+ console.log("first number:",number1)
+    console.log("second number:",number2)
+    console.log("sum:",result);
