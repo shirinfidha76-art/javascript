8e8b598cd17c8c2da8218df734ca8d5e8579cc74
Fidha Shirin <shirinfidha76@gmail.com>
2026-07-07 11:12:09 +0530

    function added

---
+++ function.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(){
+    let number1=30
+    let number2=50
+    let sum=number1+number2
+
+    console.log("first number:",number1)
+    console.log("second number:",number2)
+    console.log("sum:",sum)
+
+}
+addTwoNumbers()
