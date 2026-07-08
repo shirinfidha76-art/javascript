687765837e7c5d63a727d914ec05ad8d137791b9
Fidha Shirin <shirinfidha76@gmail.com>
2026-07-08 11:43:04 +0530

    dom

---
+++ scriptt.js
@@ -0,0 +1,6 @@
+function changeBackground(){
+    document.body.style.backgroundColor="lightgreen"
+}
+function changeText() {
+    document.getElementById("heading").textContent = "Hi this is Fidha Shirin";
+}
