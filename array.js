9111ec8765e45b744ae1ac5a9ce8f5cf401a040f
Fidha Shirin <shirinfidha76@gmail.com>
2026-07-08 11:18:47 +0530

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+//Array
+
+let studentss=["John","Jacob","Sini"]
+console.log(studentss)
+console.log(studentss[1])
+ studentss[2]="George"
+ console.log(studentss)
+
+ studentss.push("Emma")
+ console.log(studentss)
+ 
+ studentss.pop()
+ console.log(studentss)
+
+ console.log(studentss.length)
+
